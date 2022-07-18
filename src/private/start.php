65985e85bdc50fe82e95c6ad9e913e14658c7f4a
Adam Sturge <adamjsturge@gmail.com>
2022-07-18 04:41:41 -0400

    Move autoloader

--- src/private/start.php
+++ src/private/start.php
@@ -1,11 +1,14 @@
 <?php
+
+require_once(dirname(__FILE__). '/../vendor/autoload.php');
+
 ini_set('display_errors', '1');
 ini_set('error_log', '/var/log/php_error_log');
 error_reporting(E_ALL &~ E_STRICT);
 
 function connect_twig()
 {
-    require_once(dirname(__FILE__). '/../vendor/autoload.php');
+    
 
     $options = array(
         'path' => '../public/twig',
