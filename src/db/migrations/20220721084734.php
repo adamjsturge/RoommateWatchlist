3a26b9032561099a58a179b91cfe34d1e1e8a630
Adam Sturge <adamjsturge@gmail.com>
2022-07-21 05:11:25 -0400

    TV Show Table

---
+++ src/db/migrations/20220721084734.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class V20220721084734 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $table = $this->table('tv_shows');
+        $table->addColumn('user_id', 'integer')
+            ->addColumn('tv_show', 'string')
+            ->addColumn('rating', 'integer')
+            ->create();
+    }
+}
