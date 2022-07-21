226441288bb3e59f5b75e31c1411a7adede2c673
Collin Hovey <collinhovey@gmail.com>
2022-07-21 01:53:29 -0700

    More Profile Additions

--- src/private/pages/profile.php
+++ src/private/pages/profile.php
@@ -1,11 +1,19 @@
 <?php
 
+include_once(__DIR__ . '/../library/maindb.php');
+include_once(__DIR__ . '/../library/userauth.php');
+
+use AS\library\maindb;
 use AS\infra\page;
 
 class profile extends page {
 
     protected function request() {  
         $this->set_template('profile');
-        $this->data['thing'] = 'profile';
+
+        $db = new maindb;
+        $row = $db->query("SELECT email FROM users WHERE username = 'rarson'");
+        $results = $row->fetchColumn();
+        $this->data['email'] = $results;
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -12,7 +12,7 @@ $whitelist = [ //Key is the url and value is the classname of file
     'login' => 'login',
     'shop' => 'shop',
     'signup' => 'signup',
-    'profilt' => 'profile'
+    'profile' => 'profile'
 ];
 
 
