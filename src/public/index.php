75d04c1a76bf5ab7102ea238b2d3b811b245166c
adamjsturge <adamjsturge@gmail.com>
2022-07-23 16:19:49 -0400

    Add API to router

--- src/public/index.php
+++ src/public/index.php
@@ -5,6 +5,10 @@ require_once(dirname(__FILE__). '/../vendor/autoload.php');
 
 session_start(); //session_destroy(); To logout someone
 
+$is_api = str_starts_with($_SERVER['REQUEST_URI'], '/api/');
+
+$magic_number = $is_api ? 5 : 1;
+
 $request = substr($_SERVER['REQUEST_URI'], 1);
 
 $whitelist = [ //Key is the url and value is the classname of file
@@ -17,9 +21,18 @@ $whitelist = [ //Key is the url and value is the classname of file
     'my_groups' => 'my_groups'
 ];
 
+$api_whitelist = [ //Key is the url and value is the classname of file
+    'add_group_member' => 'add_group_member',
+    'add_group' => 'add_group',
+];
+
 
 $not_found = false;
-if (empty($whitelist[$request])) {
+if (!$is_api && empty($whitelist[$request])) {
+    $not_found = true;
+}
+
+if ($is_api && empty($api_whitelist[$request])) {
     $not_found = true;
 }
 $classname = $whitelist[$request]?? 'not_found';
@@ -28,11 +41,19 @@ $classname = $whitelist[$request]?? 'not_found';
 // }
 
 if (!$not_found) {
-    require_once(__DIR__ . "/../private/pages/$request.php");
+    if ($is_api) {
+        require_once(__DIR__ . "/../private/api/$request.php");
+    } else {
+        require_once(__DIR__ . "/../private/pages/$request.php");
+    }
     $page = new $classname; 
     $page->run();
 } else {
-    require_once(__DIR__ . "/../private/pages/404.php");
-    $page = new not_found; 
-    $page->run();
+    if ($is_api) {
+        echo json_encode(['error' => 'API not found.']);
+    } else {
+        require_once(__DIR__ . "/../private/pages/404.php");
+        $page = new not_found; 
+        $page->run();
+    }
 }
